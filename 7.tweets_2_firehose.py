3e718c3035f0b4e27098986c0c7a8637df786db7
Charlotte1904 <chaudao19@gmail.com>
2017-02-06 15:16:58 -0800

    Add files via upload

---
+++ 7.tweets_2_firehose.py
@@ -0,0 +1,28 @@
+import boto3
+import os
+import yaml
+import json
+from twitter import TwitterStream, OAuth
+
+
+# Connect to firehose
+client = boto3.client('firehose',region_name = 'us-west-2')
+
+# Connect to Twitter
+credentials = yaml.load(open(os.path.expanduser( 'twitter_cred.yml')))
+twitter_stream = TwitterStream(auth = OAuth(**credentials['twitter']))
+
+#Streamming Tweets
+iterator = twitter_stream.statuses.sample()
+
+#Loading tweets to the firehose
+counter = 0
+for tweet in iterator:
+	if "delete" not in tweet:
+		response = client.put_record(DeliveryStreamName='chau_firehose',
+			Record={'Data': json.dumps(str(tweet)+'\n')})
+		counter += 1
+	if counter % 50 == 0:
+		print('Inserted {} tweets'.format(counter))
+
+
