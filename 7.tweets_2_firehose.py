fcbe7467bcce9eb930db0b2efa3a4bda274d992a
Charlotte1904 <chaudao19@gmail.com>
2017-02-22 14:23:13 -0800

    Update 7.tweets_2_firehose.py

--- 7.tweets_2_firehose.py
+++ 7.tweets_2_firehose.py
@@ -1,3 +1,7 @@
+# 1. Create a kinesis firehose - Specify/Create S3 Bucket 
+# 2. Create EC2 Instance - stream the data
+
+
 import boto3
 import os
 import yaml
@@ -13,16 +17,21 @@ credentials = yaml.load(open(os.path.expanduser( 'twitter_cred.yml')))
 twitter_stream = TwitterStream(auth = OAuth(**credentials['twitter']))
 
 #Streamming Tweets
-iterator = twitter_stream.statuses.sample()
+
 
 #Loading tweets to the firehose
-counter = 0
-for tweet in iterator:
-	if "delete" not in tweet:
-		response = client.put_record(DeliveryStreamName='chau_firehose',
-			Record={'Data': json.dumps((tweet)+'\n')})
-		counter += 1
-	if counter % 50 == 0:
-		print('Inserted {} tweets'.format(counter))
+def firehose(twitter_stream):
+	iterator = twitter_stream.statuses.sample()
+	counter = 0
+	for tweet in iterator:
+		if "delete" not in tweet:
+			response = client.put_record(DeliveryStreamName='chau_firehose',
+				Record={'Data': json.dumps((tweet)+'\n')})
+			counter += 1
+		if counter % 50 == 0:
+			print('Inserted {} tweets'.format(counter))
 
+if __name__ == 	'__main__':	
+	while True:
+		firehose(twitter_stream)
 
